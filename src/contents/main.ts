8073d77c20df0cc0fb2c2bb89d5d84a610c9ae35
Kaan <quirxsama@proton.me>
2026-04-30 15:55:41 +0300

    Add story indicator and external reel link options

--- src/contents/main.ts
+++ src/contents/main.ts
@@ -1,9 +1,12 @@
 import type { PlasmoCSConfig } from "plasmo"
 
+import { Storage } from "@plasmohq/storage"
+
 import { Global, Reels, Stories } from "~modules/instagram"
 
 export const config: PlasmoCSConfig = {
-  matches: ["https://www.instagram.com/*"]
+  matches: ["https://www.instagram.com/*"],
+  run_at: "document_start"
 }
 
 const REGEX =
@@ -11,9 +14,37 @@ const REGEX =
 const global = new Global()
 const reels = new Reels()
 const stories = new Stories()
+const storage = new Storage()
 
 let previousUrl = ""
+let openExternalReelsAsPosts = false
+
+storage.get("bigv-open-external-reels-as-posts").then((value) => {
+  openExternalReelsAsPosts = value ?? false
+  openExternalReelAsPost()
+})
+storage.watch({
+  "bigv-open-external-reels-as-posts": (c) => {
+    openExternalReelsAsPosts = c.newValue
+    load()
+  }
+})
+
+const openExternalReelAsPost = () => {
+  if (!openExternalReelsAsPosts) return
+
+  const [, id] = location.pathname.match(/^\/reel\/([^/]+)\/?$/i) ?? []
+  if (!id) return
+
+  const referrer = document.referrer ? new URL(document.referrer) : null
+  if (referrer?.hostname.endsWith("instagram.com")) return
+
+  location.replace(`/p/${id}/${location.search}`)
+}
+
 const load = () => {
+  openExternalReelAsPost()
+
   const match = location.pathname.match(REGEX)
   const first = match?.[1]
   switch (first) {
